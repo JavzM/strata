8950d68d48fb0cfd788737dc4449f18894dc443e
Javier <javier.martinez.blanco@est.una.ac.cr>
2026-01-12 23:05:12 -0600

    Adding analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {Analytics} from "@vercel/analytics/react"
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import {ThemeProvider} from "@/providers/theme-provider.tsx";
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-          <App />
-      </ThemeProvider>
-  </StrictMode>,
+    <StrictMode>
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+            <App/>
+        </ThemeProvider>
+        <Analytics/>
+    </StrictMode>
 )
