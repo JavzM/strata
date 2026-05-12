0e82912e38d76b73916edc9a03405256b1988934
Javier <javier.martinez.blanco@est.una.ac.cr>
2026-05-11 18:04:58 -0600

    adding new routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,21 @@
 import {Analytics} from "@vercel/analytics/react"
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {ThemeProvider} from "@/providers/theme-provider.tsx";
+import LandingPage from "@/pages/LandingPage.tsx";
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-            <App/>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<LandingPage/>}/>
+                    <Route path="/generate" element={<App/>}/>
+                </Routes>
+            </BrowserRouter>
         </ThemeProvider>
         <Analytics/>
     </StrictMode>
